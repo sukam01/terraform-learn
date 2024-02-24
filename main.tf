aa51e057656069624b1eb0c522d8cb4ee586371c
su-kamal <64933081+su-kamal@users.noreply.github.com>
2024-02-23 21:28:45 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,25 @@
 provider "aws" {
-   region = "us-east-2"
-   access_key = "AKIA6GBMHDARAOKMLKXB"
-   secret_key = "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"
+   region = "us-west-2"
    }
+   
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-resource "aws_vpc" "development-vpc"{
-   cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "myapp-vpc"{
+   cidr_block = var.vpc_cidr_block
+   tags = {
+	Name: "${var.env_prefix}-vpc"
+	}
 }
 
-resource "aws_subnet" "dev-subnet-1"{
-   vpc_id = aws_vpc.development-vpc.id
-   cidr_block = "10.0.10.0/24"
-   availability_zone = "us-east-2a"
+resource "aws_subnet" "myapp-subnet-1"{
+   vpc_id = aws_vpc.myapp-vpc.id
+   cidr_block = var.subnet_cidr_block
+   availability_zone = var.avail_zone
+   tags = {
+	Name: "${var.env_prefix}-subnet-1"
+	}
 }
    
