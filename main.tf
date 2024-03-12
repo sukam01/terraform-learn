afb8dd4cf4d4c3419176bfb2bf2f134b716f79cd
su-kamal <64933081+su-kamal@users.noreply.github.com>
2024-03-12 08:19:20 -0700

    Add docker

--- main.tf
+++ main.tf
@@ -22,4 +22,26 @@ resource "aws_subnet" "myapp-subnet-1"{
 	Name: "${var.env_prefix}-subnet-1"
 	}
 }
-   
+
+resource "aws_route_table" "myapp-route-table" {
+		vpc_id = aws_vpc.myapp-vpc.id
+		route {
+			cidr_block = "0.0.0.0/0"
+			gateway_id = aws_internet_gateway.myapp-igw.id
+		}
+		tags = {
+			Name: "${var.env_prefix}-rtb"
+		}
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+	vpc_id = aws_vpc.myapp-vpc.id
+	tags = {
+		Name: "${var.env_prefix}-igw"
+	}
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+	subnet_id = aws_subnet.myapp-subnet-1.id
+	route_table_id = aws_route_table.myapp-route-table.id
+}
