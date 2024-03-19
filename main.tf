b5eb03c2975675b0b37efcaa04368b493b2f3810
su-kamal <64933081+su-kamal@users.noreply.github.com>
2024-03-19 08:43:12 -0700

    Add changes

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 provider "aws" {
    region = "us-west-2"
    }
-   
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-
+  
 resource "aws_vpc" "myapp-vpc"{
    cidr_block = var.vpc_cidr_block
    tags = {
@@ -14,32 +9,24 @@ resource "aws_vpc" "myapp-vpc"{
 	}
 }
 
-resource "aws_subnet" "myapp-subnet-1"{
-   vpc_id = aws_vpc.myapp-vpc.id
-   cidr_block = var.subnet_cidr_block
-   availability_zone = var.avail_zone
-   tags = {
-	Name: "${var.env_prefix}-subnet-1"
-	}
-}
+module "myapp-subnet" {
+	source = "modules/subnet"
+	subnet_cidr_block = var.subnet_cidr_block
+{
 
-resource "aws_route_table" "myapp-route-table" {
-		vpc_id = aws_vpc.myapp-vpc.id
-		route {
-			cidr_block = "0.0.0.0/0"
-			gateway_id = aws_internet_gateway.myapp-igw.id
-		}
-		tags = {
-			Name: "${var.env_prefix}-rtb"
-		}
+module "myapp-server" }
+	source = "./modules/webserver"
+	variable vpc_id {}
+	variable my_ip {}
+	variable env_prefix {}
+	variable image_name {}
+	variable public_key_location {}
+	variable intance_type {}
+	variable subnet_id {}
+	variable default_sg_id {}
+	variable avail_zone {}
 }
-
-resource "aws_internet_gateway" "myapp-igw" {
-	vpc_id = aws_vpc.myapp-vpc.id
-	tags = {
-		Name: "${var.env_prefix}-igw"
-	}
-}
+	
 
 resource "aws_route_table_association" "a-rtb-subnet" {
 	subnet_id = aws_subnet.myapp-subnet-1.id
